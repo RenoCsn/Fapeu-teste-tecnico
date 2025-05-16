4d715887c285ca5afcb77d8b250a6929c3fab53a
RenoCsn <csnt.ren@gmail.com>
2025-05-16 16:52:06 -0300

    fix: remove commented code

--- src/Components/List/index.tsx
+++ src/Components/List/index.tsx
@@ -1,7 +1,6 @@
 import { List as AntdList, Flex } from 'antd';
 import ListItem from './ListItem';
 import type { DataType } from '../../Utils/types';
-import { useEffect, useState } from 'react';
 
 interface ListProps  {
   data: [],
@@ -10,37 +9,12 @@ interface ListProps  {
 
 const List: React.FC<ListProps> = ({data, isLoading}) => {
 
-// const [response, setResponse] = useState([]);
-// const [error, setError] = useState("");
-// const [isLoading, setIsLoading] = useState(false)
-
-//   useEffect(() => {
-    
-//     const fetchData = async () => {
-//       try{
-
-//       setIsLoading(true)
-//       const dataFetch = await fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json());
-//       setResponse(dataFetch);
-//       // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-//       } catch (error: any) {
-//         setError(error.message)
-//       } finally {
-//         setIsLoading(false)
-//       }
-//     }
-
-//     fetchData();
-
-//   }, []);
-
     return (
       <Flex gap="small" vertical style={{maxWidth: '1400px'}}>
         <AntdList 
           size="large"
           bordered
           loading={isLoading}
-          // dataSource={response}
           dataSource={data}
           renderItem={(item: DataType) => <AntdList.Item>
             <ListItem 
